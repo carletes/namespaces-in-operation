f4b70e89c96d31eb1de18910330806bbf2aa77f0
Carlos Valiente <carlos@pepelabs.net>
2020-01-20 11:54:50 +0000

    Initial commit.

--- demo_uts_namespaces/src/main.rs
+++ demo_uts_namespaces/src/main.rs
@@ -2,64 +2,24 @@ extern crate nix;
 
 use nix::sched::{clone, CloneFlags};
 use nix::sys::signal::Signal;
+use nix::sys::utsname::uname;
+use nix::sys::wait::waitpid;
+use nix::unistd::sethostname;
 use std::env;
 use std::process;
+use std::thread;
 
 const STACK_LENGTH: usize = 1024 * 1024;
 
-// static int              /* Start function for cloned child */
-//     childFunc(void *arg)
-// {
-//     struct utsname uts;
-
-//     /* Change hostname in UTS namespace of child */
-//     if (sethostname(arg, strlen(arg)) == -1)
-//         errExit("sethostname");
-
-//     /* Retrieve and display hostname */
-//     if (uname(&uts) == -1)
-//         errExit("uname");
-//     printf("uts.nodename in child:  %s\n", uts.nodename);
-
-//     /* Keep the namespace open for a while, by sleeping.
-//     This allows some experimentation--for example, another
-//     process might join the namespace. */
-//     sleep(100);
-
-//     return 0;           /* Terminates child */
-// }
-
 fn child_func(hostname: &String) -> isize {
+    sethostname(hostname).expect("sethostname() failed");
+    let uts = uname();
+    println!("uts.nodename in child: {}", uts.nodename());
+    thread::sleep_ms(100 * 1000);
     return 0;
 }
 
 fn main() {
-    // pid_t child_pid;
-    // struct utsname uts;
-
-    // /* Create a child that has its own UTS namespace;
-    //    the child commences execution in childFunc() */
-    // child_pid = clone(childFunc,
-    //                 child_stack + STACK_SIZE,   /* Points to start of
-    //                                                downwardly growing stack */
-    //                 CLONE_NEWUTS | SIGCHLD, argv[1]);
-    // if (child_pid == -1)
-    //     errExit("clone");
-    // printf("PID of child created by clone() is %ld\n", (long) child_pid);
-
-    // /* Parent falls through to here */
-    // sleep(1);           /* Give child time to change its hostname */
-    // /* Display the hostname in parent's UTS namespace. This will be
-    //    different from the hostname in child's UTS namespace. */
-    // if (uname(&uts) == -1)
-    //     errExit("uname");
-    // printf("uts.nodename in parent: %s\n", uts.nodename);
-
-    // if (waitpid(child_pid, NULL, 0) == -1)      /* Wait for child */
-    //     errExit("waitpid");
-    // printf("child has terminated\n");
-
-    // exit(EXIT_SUCCESS);
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Usage: {} <child-hostname>", args[0]);
@@ -68,12 +28,22 @@ fn main() {
 
     let mut child_stack: [u8; STACK_LENGTH] = [0; STACK_LENGTH];
 
-    clone(
+    let pid = clone(
         Box::new(|| child_func(&args[1])),
         &mut child_stack,
         CloneFlags::CLONE_NEWUTS,
         Some(Signal::SIGCHLD as i32),
-    );
+    )
+    .expect("clone() failed");
+    println!("PID of child created by clone(): {}", pid);
+
+    thread::sleep_ms(1 * 1000);
+
+    let uts = uname();
+    println!("uts.nodename in parent: {}", uts.nodename());
+
+    waitpid(pid, None).expect("waitpid() failed");
+    println!("child has terminated");
 
     process::exit(0);
 }
