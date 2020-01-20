ae28fd82933848f10248a532850b2a783d9d1228
Carlos Valiente <carlos@pepelabs.net>
2020-01-20 11:28:09 +0000

    Initial commit.

--- demo_uts_namespaces/src/main.rs
+++ demo_uts_namespaces/src/main.rs
@@ -1,3 +1,63 @@
+use std::env;
+use std::process;
+use std::thread;
+
+extern crate nix;
+
+use nix::sched::{clone, CloneFlags};
+use nix::sys::signal::Signal;
+use nix::sys::utsname::uname;
+use nix::sys::wait::waitpid;
+use nix::unistd::sethostname;
+
+const STACK_LENGTH: usize = 1024 * 1024;
+
+fn child_func(hostname: &String) -> isize {
+    // Change hostname in UTS namespace of child
+    sethostname(hostname).expect("sethostname() failed");
+
+    let uts = uname();
+    println!("uts.nodename in child: {}", uts.nodename());
+
+    // Keep the namespace open for a while, by sleeping. This allows some
+    // experimentation --- for example, another process might join the
+    // namespace.
+    thread::sleep_ms(100 * 1000);
+
+    // Terminates child.
+    return 0;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <child-hostname>", args[0]);
+        process::exit(1);
+    }
+
+    let mut child_stack: [u8; STACK_LENGTH] = [0; STACK_LENGTH];
+
+    // Create a child that has its own UTS namespace; the child commences
+    // execution in `child_func` above.
+    let pid = clone(
+        Box::new(|| child_func(&args[1])),
+        &mut child_stack,
+        CloneFlags::CLONE_NEWUTS,
+        Some(Signal::SIGCHLD as i32),
+    )
+    .expect("clone() failed");
+    println!("PID of child created by clone(): {}", pid);
+
+    // Give child time to change its hostname.
+    thread::sleep_ms(1 * 1000);
+
+    // Display the hostname in parent's UTS namespace. This will be different
+    // from the hostname in child's UTS namespace.
+    let uts = uname();
+    println!("uts.nodename in parent: {}", uts.nodename());
+
+    waitpid(pid, None).expect("waitpid() failed");
+    println!("child has terminated");
+
+    process::exit(0);
 }
