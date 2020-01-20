d719c704ede41118a0fab3ca1e75673e98821ce0
Carlos Valiente <carlos@pepelabs.net>
2020-01-20 20:39:28 +0000

    Initial commit.

--- ns_exec/src/main.rs
+++ ns_exec/src/main.rs
@@ -1,29 +1,31 @@
 use std::env;
 use std::ffi::CString;
-use std::fs::File;
+use std::fs::OpenOptions;
 use std::os::unix::io::AsRawFd;
 use std::process;
 
 extern crate nix;
 
+use nix::sched::{setns, CloneFlags};
 use nix::unistd::execvp;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        eprintln!(
-            "Usage: {} /proc/<pid>/ns/<ns-file> <cmd> [<arg> ..]",
-            args[0]
-        );
+        eprintln!("Usage: {} /proc/<pid>/ns/<ns-file> <cmd>", args[0]);
         process::exit(1);
     }
 
-    let fd = File::open(&args[1]).expect("open() failed").as_raw_fd();
+    // Get descriptor for namespace.
+    let fd = OpenOptions::new()
+        .read(true)
+        .open(&args[1])
+        .expect("open() failed");
 
-    // if (setns(fd, 0) == -1)         /* Join that namespace */
-    //     errExit("setns");
+    // Join that namespace.
+    setns(fd.as_raw_fd(), CloneFlags::empty()).expect("setns() failed");
 
-    let prog_c = CString::new(args[0].as_bytes()).expect("CString::new(prog) failed");
-    let prog_args_c = CString::new(args[2..]).expect("CString::new(prog_args) failed");
-    execvp(&prog_c, &[&prog_args_c]).expect("exec() failed");
+    // Execute a command in namespace
+    let prog = CString::new(args[2].as_bytes()).expect("CString::new(prog) failed");
+    execvp(&prog, &[&prog]).expect("exec() failed");
 }
