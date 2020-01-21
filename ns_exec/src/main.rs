38ea410b78bf6fae31ba689425ca78d317665ed2
Carlos Valiente <carlos@pepelabs.net>
2020-01-21 09:50:01 +0000

    Initial commit.

--- ns_exec/src/main.rs
+++ ns_exec/src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
 use std::fs::OpenOptions;
 use std::os::unix::io::AsRawFd;
 use std::process;
@@ -12,7 +12,10 @@ use nix::unistd::execvp;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
-        eprintln!("Usage: {} /proc/<pid>/ns/<ns-file> <cmd>", args[0]);
+        eprintln!(
+            "Usage: {} /proc/<pid>/ns/<ns-file> <cmd> [<arg> ..]",
+            args[0]
+        );
         process::exit(1);
     }
 
@@ -26,6 +29,12 @@ fn main() {
     setns(fd.as_raw_fd(), CloneFlags::empty()).expect("setns() failed");
 
     // Execute a command in namespace
-    let prog = CString::new(args[2].as_bytes()).expect("CString::new(prog) failed");
-    execvp(&prog, &[&prog]).expect("exec() failed");
+    let args_exec_owned: Vec<CString> = args
+        .iter()
+        .skip(2)
+        .map(|a| CString::new(a.as_bytes()).unwrap())
+        .collect();
+    let args_exec: Vec<&CStr> = args_exec_owned.iter().map(CString::as_c_str).collect();
+
+    execvp(&args_exec[0], &args_exec).expect("exec() failed");
 }
